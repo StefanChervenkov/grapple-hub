17e04479f9eced326f2822428718a9d28f14cebe
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2025-02-15 09:36:59 +0200

    Created a PrivateRoute component and added it to the private routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,6 +6,7 @@ import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 import StudentsTablePaginated from '../components/StudentsTablePaginated';
 import Logout from '../components/Logout';
+import PrivateRoute from '../components/PrivateRoute';
 
 
 const AppRoutes = () => {
@@ -14,11 +15,12 @@ const AppRoutes = () => {
 
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/classes" element={<WeeklyCalendar />} />
-            <Route path="/students" element={<StudentsTablePaginated />} />
+            <Route path="/classes" element={<PrivateRoute> <WeeklyCalendar/> </PrivateRoute>} />
+            <Route path="/students" element={<PrivateRoute> <StudentsTablePaginated /> </PrivateRoute> } />
             <Route path="/login" element={<LoginForm />}/>
             <Route path="/register" element={<RegisterForm />}/>
             <Route path="/logout" element={<Logout />}/>
+            <Route path="*" element={<HomePage/>} />
 
         </Routes>
 
