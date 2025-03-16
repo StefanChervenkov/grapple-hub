72bd65a82718e32314c5f3aea18a6ddf285677a0
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2025-03-16 16:59:20 +0200

    Deleted components
    Those components were part of the project's initial idea which was to create a BJJ Academy management system

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -23,7 +23,6 @@ const AppRoutes = () => {
 
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/classes" element={<PrivateRoute> <WeeklyCalendar /> </PrivateRoute>} />
             <Route path="/events" element={<EventList />} />
             <Route path="/add-event" element={<PrivateRoute> <AddEvent /></PrivateRoute>} />
             <Route path="events/:eventId/edit" element={<PrivateRoute> <EditEventForm /> </PrivateRoute>} />
