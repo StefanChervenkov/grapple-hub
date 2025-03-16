bc7d7b2286163e1fb86d0083787849b1bca975e0
StefanChervenkov <stefan.chervenkov.sth@gmail.com>
2025-03-16 16:20:40 +0200

    Changed the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "lucide-react";
+
 const HomePage = () => {
     return (
 
@@ -5,11 +7,16 @@ const HomePage = () => {
             <div className="text-center space-y-6">
                 <h1 className="text-4xl md:text-6xl font-bold">Welcome to Grapple Hub</h1>
                 <p className="text-lg md:text-xl text-gray-300">
-                    A central hub for BJJ events and applications.
+                    A central hub for BJJ events
                 </p>
-                <button className="px-6 py-3 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg transition">
-                    View Schedule
-                </button>
+
+
+                <Link to="/events" className="flex items-center space-x-2">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        View Events
+                    </button>
+                </Link>
+
             </div>
         </div>
 
